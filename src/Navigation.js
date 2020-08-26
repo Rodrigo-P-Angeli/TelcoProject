3d442876ec6e010b264d404c24cd5e2e0d04553e
Rodrigo-P-De-Angeli <Rodrigo-P-De-Angeli@users.noreply.github.com>
2020-08-26 10:26:21 -0300

    Criando Navegação de telas

---
+++ src/Navigation.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+export default class App extends Component {
+    render() {
+        return (
+            <NavigationContainer>
+                <Stack.Navigator headerMode='float'>
+                    <Stack.Screen>
+                        {() => { }}
+                    </Stack.Screen>
+                </Stack.Navigator>
+            </NavigationContainer >
+        )
+    }
+}
