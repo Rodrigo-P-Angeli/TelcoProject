2322d0f5532ce19fbfd5e53282cee26bb9a9f232
Rodrigo-P-De-Angeli <Rodrigo-P-De-Angeli@users.noreply.github.com>
2020-08-26 11:39:47 -0300

    Build com GradientBackGround

--- src/Navigation.js
+++ src/Navigation.js
@@ -10,8 +10,29 @@ export default class App extends Component {
     render() {
         return (
             <NavigationContainer>
-                <Stack.Navigator headerMode='float'>
-                    <Stack.Screen name="Alarmes">
+                <Stack.Navigator headerMode='screen'
+                    headerMode="screen"
+                    mode="modal"
+                    screenOptions={{
+                        headerStyle: {
+                            backgroundColor: 'transparent',
+                            elevation: 0, // remove shadow on Android
+                            shadowOpacity: 0, // remove shadow on iOS
+                            borderBottomWidth: 0,
+                            position: 'absolute', zIndex: 100, top: 0, left: 0, right: 0 
+                        },
+                        headerTintColor: '#fff',
+                        headerTitleStyle: {
+                            fontWeight: 'bold',
+                        },
+                    }
+                    }
+                >
+                    <Stack.Screen
+                        options={{
+                            title: 'Alarmes',
+                        }}
+                        name="Alarmes">
                         {() => <MenuAlarmes />}
                     </Stack.Screen>
                 </Stack.Navigator>
--- src/Screens/MenuAlarmes.js
+++ src/Screens/MenuAlarmes.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react'
-import { View } from 'react-native'
+import { View, StyleSheet } from 'react-native'
+import LinearGradient from 'react-native-linear-gradient'
 
 export default class MenuAlarm extends Component {
     render() {
         return (
-            <View>
-                
-            </View>
+            <LinearGradient start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }} colors={['#2b2680','#3393a7']} style={styles.linearGradient}>
+                <View>
+
+                </View>
+            </LinearGradient>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+    },
+    linearGradient: {
+        flex: 1,
+    }
+})
