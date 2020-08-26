4936f01eea8f3dbf76fd5613c20ba8ec6e8bdf71
Rodrigo-P-De-Angeli <Rodrigo-P-De-Angeli@users.noreply.github.com>
2020-08-26 16:35:13 -0300

    Update Grafico.js
    Colocando 2 curvas (Entrada e Saída)

--- src/Screens/Grafico.js
+++ src/Screens/Grafico.js
@@ -7,22 +7,33 @@ LogBox.ignoreLogs([
     'Non-serializable values were found in the navigation state',
 ]);
 
-const data = [50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80]
+const data1 = [50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80]
+const data2 = [50, 10, 10, 120, -4, -24, -23, 21, 35, 53, 13, 24, 50, -20, -80]
+const data = [
+    {
+        data: data1,
+        svg: { stroke: 'red' },
+    },
+    {
+        data: data2,
+        svg: { stroke: 'green' },
+    },
+]
+
 
 export default class Grafico extends Component {
+    static navigationOptions = {
+        title: 'Home',
+    };
     render() {
         return (
             <LinearGradient start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }} colors={['#2b2680', '#3393a7']} style={styles.linearGradient}>
-                <Text>
-                    <Text>{this.props.route.params.alarm_name}</Text>
-                </Text>
                 <LineChart
                     style={styles.chart}
                     data={data}
-                    svg={{ stroke: 'rgb(134, 65, 244)' }}
                     contentInset={{ top: 20, bottom: 20 }}
                 >
-                    <Grid />
+                    <Grid svg={{ stroke: 'white', strokeWidth: 0.2, }} />
                 </LineChart>
             </LinearGradient>
         )
@@ -33,10 +44,11 @@ const styles = StyleSheet.create({
     linearGradient: {
         flex: 1,
         paddingTop: 50,
+        justifyContent: 'center'
     },
     chart: {
         height: 200,
-        width: '80%', 
-        alignSelf: 'center'
+        width: '80%',
+        alignSelf: 'center',
     },
 })
