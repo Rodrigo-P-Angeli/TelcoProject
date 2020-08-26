a787181e3b7b98ab6e7b4cc57b424f9d85810cbe
Rodrigo-P-De-Angeli <Rodrigo-P-De-Angeli@users.noreply.github.com>
2020-08-26 16:35:40 -0300

    Update Navigation.js
    editando header

--- src/Navigation.js
+++ src/Navigation.js
@@ -8,6 +8,18 @@ import CommonStyles from './CommonStyles';
 
 const Stack = createStackNavigator();
 
+const config = {
+    animation: 'spring',
+    config: {
+        stiffness: 1000,
+        damping: 500,
+        mass: 3,
+        overshootClamping: true,
+        restDisplacementThreshold: 0.01,
+        restSpeedThreshold: 0.01,
+    },
+};
+
 export default class App extends Component {
     render() {
         return (
@@ -17,17 +29,36 @@ export default class App extends Component {
                         headerTransparent: true,
                         headerStyle: {
                             height: 50,
+
                         },
                         headerTintColor: '#fff',
-                        headerTitleStyle: {
-                            fontFamily: CommonStyles.fontFamilyTitle,
-                            alignSelf: 'center',
-                            fontSize: 20,
-                        },
                     }}
                     initialRouteName='Alarmes'>
-                    <Stack.Screen name="Alarmes" component={MenuAlarmes} />
-                    <Stack.Screen name="Grafico" component={Grafico} />
+                    <Stack.Screen name="Alarmes" component={MenuAlarmes}
+                        options={({ route }) => ({
+                            headerTitleStyle: {
+                                fontFamily: CommonStyles.fontFamilyTitle,
+                                alignSelf: 'flex-start',
+                                fontSize: 20,
+                                marginLeft: 50,
+                            },
+                            gestureDirection: 'vertical',
+                            transitionSpec: {
+                                open: config,
+                                close: config,
+                            },
+                        })} />
+                    <Stack.Screen name="Grafico" component={Grafico}
+                        options={({ route }) => ({
+                            title: route.params.alarm_name,
+                            headerTitleStyle: {
+                                fontFamily: CommonStyles.fontFamilyTitle,
+                                alignSelf: 'flex-start',
+                                fontSize: 20,
+                            },
+                            gestureDirection: 'horizontal',
+                        })}
+                    />
                 </Stack.Navigator>
             </NavigationContainer >
         )
