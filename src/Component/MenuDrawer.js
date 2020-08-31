fd708bf96e2a6401699493f975652fc1143d7bf9
Rodrigo-P-De-Angeli <Rodrigo-P-De-Angeli@users.noreply.github.com>
2020-08-31 11:08:20 -0300

    Update MenuDrawer.js

--- src/Component/MenuDrawer.js
+++ src/Component/MenuDrawer.js
@@ -1,30 +1,33 @@
 import React, { Component } from 'react'
 import { View, Text, Image, StyleSheet } from 'react-native'
-import {
-    DrawerContentScrollView,
-    DrawerItemList,
-} from '@react-navigation/drawer'
-
+import { DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer'
+import { HeaderBackButton } from '@react-navigation/stack';
 import Email from 'react-native-vector-icons/MaterialCommunityIcons'
+import LinearGradient from 'react-native-linear-gradient'
+
 import CommonStyles from '../CommonStyles'
 
 
 export default class MenuDrawer extends Component {
     render() {
         return (
-            <View style={{ flex: 1, backgroundColor: '#47479F', elevation: 10, }}>
+            <View style={{ flex: 1, backgroundColor: '#000066', elevation: 10, }}>
                 <View style={styles.container}>
-                    <Text style={styles.title}></Text>
+                    <LinearGradient start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }} colors={['#000066', '#47479F']} style={styles.linearGradient}>
+                        <HeaderBackButton
+                            tintColor={CommonStyles.Colors.white}
+                            onPress={() => this.props.navigation.closeDrawer()} />
+                        <Text style={styles.title}>Menu</Text>
+                    </LinearGradient>
+                    <Text style={styles.title}>Telcomanager</Text>
                     <View style={styles.content}>
-                        <View style={{ flex: 1, justifyContent: 'space-around' }}>
-                            <View style={styles.itens}>
-                                <Email name={'email'} size={15} color={CommonStyles.Colors.white} />
-                                <Text style={styles.contato}>rodrigop.deangeli@gmail.com</Text>
-                            </View>
-                            <View style={styles.itens}>
-                                <Email name={'phone'} size={15} color={CommonStyles.Colors.white} />
-                                <Text style={styles.contato}>+55 27 99992-8105</Text>
-                            </View>
+                        <View style={styles.itens}>
+                            <Email name={'email'} size={15} color={CommonStyles.Colors.white} />
+                            <Text style={styles.contato}>rodrigop.deangeli@gmail.com</Text>
+                        </View>
+                        <View style={styles.itens}>
+                            <Email name={'phone'} size={15} color={CommonStyles.Colors.white} />
+                            <Text style={styles.contato}>+55 27 99992-8105</Text>
                         </View>
                     </View>
                 </View>
@@ -37,18 +40,21 @@ export default class MenuDrawer extends Component {
 }
 
 const styles = StyleSheet.create({
-    image: {
-        width: 50,
-        height: 50,
-        borderRadius: 25,
-        resizeMode: 'contain',
-        alignSelf: 'center',
+    content: {
+        justifyContent: 'space-around',
+        padding: 15,
+        paddingTop: 0,
     },
     title: {
         fontSize: 20,
-        padding: 10,
         fontFamily: CommonStyles.fontFamilyTitle,
         color: CommonStyles.Colors.white,
+        padding: 15,
+    },
+    linearGradient: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 0,
     },
     contato: {
         fontSize: 13,
@@ -57,16 +63,11 @@ const styles = StyleSheet.create({
         color: CommonStyles.Colors.white,
     },
     container: {
-        padding: 10,
+        paddingBottom: 10,
         borderBottomColor: CommonStyles.Colors.white,
         borderBottomWidth: 1,
     },
-    content: {
-        flexDirection: 'row',
-        padding: 5,
-    },
     itens: {
         flexDirection: 'row',
-        paddingLeft: 10,
     },
 })
