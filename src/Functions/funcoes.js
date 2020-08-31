0709fd60ffdb25dd436c4df71afb2af123da09b3
Rodrigo-P-De-Angeli <Rodrigo-P-De-Angeli@users.noreply.github.com>
2020-08-31 16:12:10 -0300

    Update funcoes.js
    Ajustando gráfico para arredondar hora dos 5 min para baixo, sempre.

--- src/Functions/funcoes.js
+++ src/Functions/funcoes.js
@@ -12,7 +12,7 @@ export function generateTime() {
     const dado = []
     const date = new Date()
     const coff = 1000 * 60 * 5
-    const agora = Math.round(date.getTime() / coff) * coff
+    const agora = Math.floor(date.getTime() / coff) * coff
     for (let i = 0; i < 288; i++) {
         if ((i + 32) % 32 == 0 || i == 287) {
             let hora = dateFns.addHours(agora, -(288 - i) / 288 * 24)
